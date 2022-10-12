ccbc57e456a4daa9a5ae0ccfdfd1d104e1e37659
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-10-11 20:26:33 -0700

    Added fetch function for geocoding for input.

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,27 +27,70 @@ setInterval(currentTimeShown, 1000);
 // }
 
 
+var geocodingLat = [];
+var geocodingLon = [];
+
+var weatherUrl;
+var geocodingUrl;
+var geocodingObject ;
+
+
+var locationEntry;
+
+
 $('#submit').on('click', function (event) {
     event.preventDefault();
 
-    var locationEntry = locationSearch.val().toUpperCase().trim();
-
     searchedCities = JSON.parse(localStorage.getItem("searchedCitiesStringify"));
-    if (locationEntry === '') {
-        alert('Invalid city input!');
-    }
-    else {
-        if (!searchedCities.includes(locationEntry)) {
-            searchedCities.push(locationEntry);
-            console.log(searchedCities);
-            localStorage.setItem("searchedCitiesStringify", JSON.stringify(searchedCities));
-            console.log(searchedCities);
 
-            $("#enteredCity").append("<button>" + locationEntry + "</button>");
-            $("#enteredCity").children().attr("class", "row btn btn-primary m-1 mb-2 w-100");
+    locationEntry = locationSearch.val().toUpperCase().trim();
 
+    geocodingUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + locationEntry + '&limit=1&appid=3cbcebba60d585a203f6e51f482d9176';
+
+    fetch(geocodingUrl).then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            if(data.length===0){
+                alert("Invalid city input!");    
+            }
+            else{
+                console.log(data[0].lon + "   " + data[0].country );
+                // weatherUrl = '' + data[0].lon + data[0].lat + "";
+                if (!searchedCities.includes(locationEntry)) {
+                    searchedCities.push(locationEntry);
+                    // console.log(searchedCities);
+                    localStorage.setItem("searchedCitiesStringify", JSON.stringify(searchedCities));
+                    // console.log(searchedCities);
+        
+                    $("#enteredCity").append("<button>" + locationEntry + "</button>");
+                    $("#enteredCity").children().attr("class", "row btn btn-primary m-1 mb-2 w-100");
+        
+                }
+                //geocodingObject = data[0];
+            }
+          });
+        } else {
+            alert("Invalid city input!");
         }
-    }
+      });
+
+    // console.log(geocodingObject.lon + " ");
+
+    // if (locationEntry === '') {
+    //     alert('Invalid city input!');
+    // }
+    // else {
+    //     if (!searchedCities.includes(locationEntry)) {
+    //         searchedCities.push(locationEntry);
+    //         // console.log(searchedCities);
+    //         localStorage.setItem("searchedCitiesStringify", JSON.stringify(searchedCities));
+    //         // console.log(searchedCities);
+
+    //         $("#enteredCity").append("<button>" + locationEntry + "</button>");
+    //         $("#enteredCity").children().attr("class", "row btn btn-primary m-1 mb-2 w-100");
+
+    //     }
+    // }
 
     locationSearch.val("");
 });
@@ -60,9 +103,16 @@ $('#clear').on('click', function (event) {
     searchedCities = [];
     if (localStorage.getItem("searchedCitiesStringify") === null) {
         localStorage.setItem("searchedCitiesStringify", JSON.stringify(searchedCities));
-    
+
         // $("#enteredCity").remove();
         $("#searchBox").val('');
         $("#enteredCity").empty();
     }
 });
+
+// $("enteredCity").click(function (event) {
+//     event.preventDefault();
+// });
+
+geocodingUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + locationEntry + '&limit=1&appid=3cbcebba60d585a203f6e51f482d9176';
+console.log(locationEntry); 
